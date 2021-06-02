23cde4ff65ed08c04c787df59f36a5d0f89d0d4e
edwardsahakyanc <edwardsahakyanc1@gmail.com>
2021-06-02 12:31:16 +0400

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import './App.scss';
-import {NavLink, Route, Switch} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import ExploreGalleries from "./exploreGalleries";
 import DropMagnet from "./dropMagnet";
 
 function App() {
   return (
     <div className="App">
-        {/*<NavLink to="/explore-galleries">ExploreGalleries</NavLink>*/}
-        {/*<NavLink to="/drop-magnet/artgallery.link">DropMagnet artgallery</NavLink>*/}
-        {/*<NavLink to="/drop-magnet/artgallery.link/cryptoartman">DropMagnet cryptoartman</NavLink>*/}
         <Switch>
             <Route path="/explore-galleries" component={ExploreGalleries} />
             <Route path="/drop-magnet" component={DropMagnet} />
