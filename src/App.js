0e73b693ff3e122d73f2025eee67467fad390fe7
edwardsahakyanc <edwardsahakyanc1@gmail.com>
2021-06-02 12:26:09 +0400

    route fixing

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import DropMagnet from "./dropMagnet";
 function App() {
   return (
     <div className="App">
-        <NavLink to="/explore-galleries">ExploreGalleries</NavLink>
-        <NavLink to="/drop-magnet/artgallery.link">DropMagnet artgallery</NavLink>
-        <NavLink to="/drop-magnet/artgallery.link/cryptoartman">DropMagnet cryptoartman</NavLink>
+        {/*<NavLink to="/explore-galleries">ExploreGalleries</NavLink>*/}
+        {/*<NavLink to="/drop-magnet/artgallery.link">DropMagnet artgallery</NavLink>*/}
+        {/*<NavLink to="/drop-magnet/artgallery.link/cryptoartman">DropMagnet cryptoartman</NavLink>*/}
         <Switch>
             <Route path="/explore-galleries" component={ExploreGalleries} />
             <Route path="/drop-magnet" component={DropMagnet} />
